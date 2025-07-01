367639f3c7f7be0f8803823caf5b8418484dd4e6
funeralstream <mohnach2014@mail.ru>
2025-07-01 13:50:53 +0300

    spring-boot integration

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false)
     private int age;
 
-    @CreationTimestamp
+    @CreatedDate
     @Column(nullable = false)
     private LocalDateTime created_at;
 
