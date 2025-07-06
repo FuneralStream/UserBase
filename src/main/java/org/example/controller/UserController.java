4a0eaa41842b87db862481a52ed26c81d846ce87
funeralstream <mohnach2014@mail.ru>
2025-07-07 02:20:55 +0300

    spring-boot integration

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+    //hello
 
     @Autowired
     private final UserService userService;
